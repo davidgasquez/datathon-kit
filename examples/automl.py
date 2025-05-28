9e716592056153213228b92c8a5b3b807415b1d9
David Gasquez <davidgasquez@gmail.com>
2025-05-28 12:00:33 +0200

    🧹 Update .gitignore to include AutogluonModels and clean up automl.py by removing unnecessary lines

--- examples/automl.py
+++ examples/automl.py
@@ -1,19 +1,15 @@
 import pandas as pd
 from autogluon.tabular import TabularDataset, TabularPredictor
 
+# Load data
 data_url = "https://raw.githubusercontent.com/mli/ag-docs/main/knot_theory/"
 train_data: pd.DataFrame = TabularDataset(f"{data_url}train.csv")
-train_data.head()
 
+# Train model
 label = "signature"
-train_data[label].describe()
+predictor = TabularPredictor(label=label).fit(train_data)
 
-
-predictor = TabularPredictor(label=label).fit(
-    train_data,
-    ag_args_fit={"num_gpus": 1},
-)
-
+# Predict
 test_data = TabularDataset(f"{data_url}test.csv")
 y_pred = predictor.predict(test_data.drop(columns=[label]))
 print(y_pred[:5])
